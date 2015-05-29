aebbf284eff464b2c18d713506ba6c809e3db759
nljb <he.liu@danoonetworks.com>
2015-05-29 14:51:50 +0800

    ...

---
+++ auto.go
@@ -0,0 +1,16 @@
+package freehttp
+
+// Json 普通格式
+type Json interface{}
+
+// Json 排版格式
+type JsonIndent interface{}
+
+// Body
+type Body interface{}
+
+// Json Body
+type BodyJson interface{}
+
+// HTTP Status
+type Status interface{}
